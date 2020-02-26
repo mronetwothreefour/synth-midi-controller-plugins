40c0ac777b8c5d4c7d4bcc3433a398987c72546f
Desktop <socl02@mr1234.com>
2020-02-26 14:04:04 -0700

    In PublicParameters.h, added code for building StringArrays pitchNameChoices and fineTuneChoices. Added osc1Pitch and osc1Fine parameters to the layout.

--- MophoGUI/Source/parameters/PublicParameters.h
+++ MophoGUI/Source/parameters/PublicParameters.h
@@ -17,9 +17,50 @@ public:
 	// This layout is then used to initialize the AudioProcessorValueTreeState.
 	ParamLayout createLayout()
 	{
+		//==============================================================================
+		// Define parameter choice StringArrays
+
+		// Used to present a MIDI note number as a pitch name / octave number String
+		// (e.g. note number 27 is presented as "D#2")
+		StringArray pitchNameChoices;
+		for (int i = 0; i != 121; ++i)
+		{
+			auto noteNum{ i % 12 };
+			auto octaveNum{ i / 12 };
+			switch (noteNum)
+			{
+			case 0 : pitchNameChoices.add("C"  + (String)octaveNum); break;
+			case 1 : pitchNameChoices.add("C#" + (String)octaveNum); break;
+			case 2 : pitchNameChoices.add("D"  + (String)octaveNum); break;
+			case 3 : pitchNameChoices.add("D#" + (String)octaveNum); break;
+			case 4 : pitchNameChoices.add("E"  + (String)octaveNum); break;
+			case 5 : pitchNameChoices.add("F"  + (String)octaveNum); break;
+			case 6 : pitchNameChoices.add("F#" + (String)octaveNum); break;
+			case 7 : pitchNameChoices.add("G"  + (String)octaveNum); break;
+			case 8 : pitchNameChoices.add("G#" + (String)octaveNum); break;
+			case 9 : pitchNameChoices.add("A"  + (String)octaveNum); break;
+			case 10: pitchNameChoices.add("A#" + (String)octaveNum); break;
+			case 11: pitchNameChoices.add("B"  + (String)octaveNum); break;
+			default: break;
+			}
+		}
+
+		// 0 = "-50 cents"; 50 = "Centered"; 100 = "+50 cents"
+		StringArray fineTuneChoices;
+		for (int i = 0; i != 50; ++i)
+		{
+			fineTuneChoices.add((String)(i - 50) + " cents");
+		}
+		fineTuneChoices.add("Centered");
+		for (int i = 51; i != 101; ++i)
+		{
+			fineTuneChoices.add("+" + (String)(i - 50) + " cents");
+		}
+		//==============================================================================
+
 		ParamLayout layout;
-		layout.add(std::make_unique<AudioParameterInt>("osc1Freq", "Oscillator 1 Pitch", 0, 120, 24));
-		layout.add(std::make_unique<AudioParameterFloat>("dingle", "Dingle", 0.0f, 1.0f, 0.5f));
+		layout.add(std::make_unique<AudioParameterChoice>("osc1Pitch", "Oscillator 1 Pitch", pitchNameChoices, 24));
+		layout.add(std::make_unique<AudioParameterChoice>("osc1Fine", "Oscillator 1 Fine Tune", fineTuneChoices, 49));
 		return layout;
 	}
 
