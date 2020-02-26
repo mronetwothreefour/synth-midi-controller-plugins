f73bc9bffb2567d9ce7278d6b621e85da2aed535
Desktop <socl02@mr1234.com>
2020-02-25 17:30:21 -0700

    Tried creating dev-mophogui branch again, this time pushed it to the remote repo upon creation. Added AudioProcessorValueTreeState and a few public parameters.

--- MophoGUI/Source/PluginProcessor.h
+++ MophoGUI/Source/PluginProcessor.h
@@ -38,6 +38,8 @@ public:
     void setStateInformation(const void* data, int sizeInBytes) override;
 
 private:
+    std::unique_ptr<AudioProcessorValueTreeState> apvts;
+
     //==============================================================================
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(PluginProcessor)
 };
