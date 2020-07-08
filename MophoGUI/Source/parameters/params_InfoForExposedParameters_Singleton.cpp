6931352762304e0c5023bd32751ea19b98a81516
Desktop <socl02@mr1234.com>
2020-07-08 13:36:53 -0600

    Added entries for these parameters to InfoForExposedParameters::fillAllInfoContainers(): osc1SubLevel, osc2Pitch, osc2Fine, osc2Shape, osc2Glide, osc2KeyTrack, and osc2SubLevel.

--- MophoGUI/Source/parameters/params_InfoForExposedParameters_Singleton.cpp
+++ MophoGUI/Source/parameters/params_InfoForExposedParameters_Singleton.cpp
@@ -81,7 +81,6 @@ void InfoForExposedParameters::fillAllInfoContainers() noexcept {
 	maxValues.add((uint8)120); 
 	defaultValues.add((uint8)24);
 	descriptions.add("Sets oscillator 1's base pitch in semitone steps.\nRange: C 0 (8 Hz) to C 10 (8 KHz). Middle C is C 5.");
-	auto dingle{ descriptions[0] };
 	ctrlWidths.add(knob_diameter);
 	ctrlHeights.add(knob_diameter);
 	ctrlCenterPoints.add(Point<int>(48, 50));
@@ -110,29 +109,113 @@ void InfoForExposedParameters::fillAllInfoContainers() noexcept {
 	ctrlHeights.add(knob_diameter);
 	ctrlCenterPoints.add(Point<int>(138, 50));
 
-	identifiers.add("osc1Glide");
+	identifiers.add("osc1Glide"); // 3
 	exposedNames.add("Oscillator 1 Glide Rate");
 	controlTypes.add(ControlType::knobWithValueStringDisplay);
 	NRPNs.add((uint16)3); 
 	converters.add(IntToPlainValueString::get());
 	maxValues.add((uint8)127); 
 	defaultValues.add((uint8)0);
-	descriptions.add("Sets the oscillator's glide (portamento) rate.\nRange: 0 (instantaneous) to 127 (very slow)");
+	descriptions.add("Sets oscillator 1's glide (portamento) rate.\nRange: 0 (instantaneous) to 127 (very slow)");
 	ctrlWidths.add(knob_diameter);
 	ctrlHeights.add(knob_diameter);
 	ctrlCenterPoints.add(Point<int>(183, 50));
 
-	identifiers.add("osc1KeyTrack");
+	identifiers.add("osc1KeyTrack"); // 4
 	exposedNames.add("Oscillator 1 Keyboard Track On/Off");
 	controlTypes.add(ControlType::toggleButton);
 	NRPNs.add((uint16)4); 
 	converters.add(IntToOffOnString::get());
 	maxValues.add((uint8)1); 
 	defaultValues.add((uint8)1);
-	descriptions.add("Turns keyboard tracking for oscillator 1\non or off. When turned off, oscillator 1\nalways produces its base pitch, unaffected\nby the pitch of incoming MIDI notes.\n");
+	descriptions.add("Turns keyboard tracking for oscillator 1\non or off. When turned off, oscillator 1\nalways produces its base pitch, unaffected\nby the pitch of incoming MIDI notes.");
 	ctrlWidths.add(toggle_diameter);
 	ctrlHeights.add(toggle_diameter);
 	ctrlCenterPoints.add(Point<int>(273, 50));
+
+	identifiers.add("osc1SubLevel"); // 5
+	exposedNames.add("Sub-Oscillator 1 Level");
+	controlTypes.add(ControlType::knobWithValueStringDisplay);
+	NRPNs.add((uint16)114); 
+	converters.add(IntToPlainValueString::get());
+	maxValues.add((uint8)127); 
+	defaultValues.add((uint8)0);
+	descriptions.add("Sets the level of sub-oscillator 1,\nwhich generates a square wave pitched\none octave lower than oscillator 1.\nRange: 0 to 127.");
+	ctrlWidths.add(knob_diameter);
+	ctrlHeights.add(knob_diameter);
+	ctrlCenterPoints.add(Point<int>(228, 50));
+
+	identifiers.add("osc2Pitch"); // 6
+	exposedNames.add("Oscillator 2 Pitch");
+	controlTypes.add(ControlType::knobWithValueStringDisplay);
+	NRPNs.add((uint16)5); 
+	converters.add(IntToOscPitchString::get()); 
+	maxValues.add((uint8)120); 
+	defaultValues.add((uint8)24);
+	descriptions.add("Sets oscillator 2's base pitch in semitone steps.\nRange: C 0 (8 Hz) to C 10 (8 KHz). Middle C is C 5.");
+	ctrlWidths.add(knob_diameter);
+	ctrlHeights.add(knob_diameter);
+	ctrlCenterPoints.add(Point<int>(48, 110));
+
+	identifiers.add("osc2Fine"); // 7
+	exposedNames.add("Oscillator 2 Fine Tune");
+	controlTypes.add(ControlType::knobWithValueStringDisplay);
+	NRPNs.add((uint16)6); 
+	converters.add(IntToFineTuneString::get());
+	maxValues.add((uint8)100); 
+	defaultValues.add((uint8)51);
+	descriptions.add("Fine tunes oscillator 2's base pitch.\nRange: -50 cents to +50 cents.\n0 = no detuning (centered).");
+	ctrlWidths.add(knob_diameter);
+	ctrlHeights.add(knob_diameter);
+	ctrlCenterPoints.add(Point<int>(93, 110));
+
+	identifiers.add("osc2Shape"); // 8
+	exposedNames.add("Oscillator 2 Wave Shape");
+	controlTypes.add(ControlType::knobWithWaveShapeDisplay);
+	NRPNs.add((uint16)7); 
+	converters.add(IntToOscWaveShapeString::get());
+	maxValues.add((uint8)103); 
+	defaultValues.add((uint8)1);
+	descriptions.add("Selects oscillator 2's wave shape.\nAvailable options: Off; Sawtooth; Triangle;\nSawtooth/Triangle Mix; Pulse (Width 0 to 99).\nA Pulse with width 50 is a square wave.");
+	ctrlWidths.add(knob_diameter);
+	ctrlHeights.add(knob_diameter);
+	ctrlCenterPoints.add(Point<int>(138, 110));
+
+	identifiers.add("osc2Glide"); // 9
+	exposedNames.add("Oscillator 2 Glide Rate");
+	controlTypes.add(ControlType::knobWithValueStringDisplay);
+	NRPNs.add((uint16)8); 
+	converters.add(IntToPlainValueString::get());
+	maxValues.add((uint8)127); 
+	defaultValues.add((uint8)0);
+	descriptions.add("Sets oscillator 2's glide (portamento) rate.\nRange: 0 (instantaneous) to 127 (very slow)");
+	ctrlWidths.add(knob_diameter);
+	ctrlHeights.add(knob_diameter);
+	ctrlCenterPoints.add(Point<int>(183, 110));
+
+	identifiers.add("osc2KeyTrack"); // 10
+	exposedNames.add("Oscillator 2 Keyboard Track On/Off");
+	controlTypes.add(ControlType::toggleButton);
+	NRPNs.add((uint16)9); 
+	converters.add(IntToOffOnString::get());
+	maxValues.add((uint8)1); 
+	defaultValues.add((uint8)1);
+	descriptions.add("Turns keyboard tracking for oscillator 2\non or off. When turned off, oscillator 1\nalways produces its base pitch, unaffected\nby the pitch of incoming MIDI notes.");
+	ctrlWidths.add(toggle_diameter);
+	ctrlHeights.add(toggle_diameter);
+	ctrlCenterPoints.add(Point<int>(273, 110));
+
+	identifiers.add("osc2SubLevel"); // 11
+	exposedNames.add("Sub-Oscillator 2 Level");
+	controlTypes.add(ControlType::knobWithValueStringDisplay);
+	NRPNs.add((uint16)115); 
+	converters.add(IntToPlainValueString::get());
+	maxValues.add((uint8)127); 
+	defaultValues.add((uint8)0);
+	descriptions.add("Sets the level of sub-oscillator 2,\nwhich generates a square wave pitched\none octave lower than oscillator 2.\nRange: 0 to 127.");
+	ctrlWidths.add(knob_diameter);
+	ctrlHeights.add(knob_diameter);
+	ctrlCenterPoints.add(Point<int>(228, 110));
 }
 
 
