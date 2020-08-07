5b4454671c5463c5c7cc632f9021db5c75193ac5
Desktop <socl02@mr1234.com>
2020-08-07 12:09:17 -0600

    In IncomingNRPNhandler, added code to pass through NRPN controller messages that are not part of a fully formed NRPN message block. Also added code to pass through messages that aren't on the channel the plugin is set to receive on.

--- MophoGUI/Source/midiTools/midi_IncomingNRPNhandler.cpp
+++ MophoGUI/Source/midiTools/midi_IncomingNRPNhandler.cpp
@@ -23,39 +23,52 @@ MidiBuffer IncomingNRPNhandler::pullFullyFormedNRPNoutOfBuffer(const MidiBuffer&
     auto nrpnTypeLSBvalue{ 0 };
     auto nrpnValueMSBvalue{ 0 };
     auto nrpnValueLSBvalue{ 0 };
+    MidiBuffer incompleteNRPN;
     MidiBuffer midiMessagesToPassThrough;
     for (auto event : midiMessages) {
+        auto& midiParams{ MidiParameters_Singleton::get() };
         auto midiMessage{ event.getMessage() };
-        if (!messageTargetsOneOfTheNRPNcontrollers(midiMessage))
+        auto messageChannel{ midiMessage.getChannel() - 1 };
+        auto pluginChannel{ midiParams.channel() };
+        if (messageChannel != pluginChannel || !messageTargetsOneOfTheNRPNcontrollers(midiMessage))
             midiMessagesToPassThrough.addEvent(midiMessage, (int)midiMessage.getTimeStamp());
         else {
             if (midiMessage.isControllerOfType(nrpnTypeMSB)) {
                 nrpnTypeMSBvalue = midiMessage.getControllerValue();
+                incompleteNRPN.addEvent(midiMessage, (int)midiMessage.getTimeStamp());
                 nrpnTypeMSBvalueReceived = true;
             }
             if (midiMessage.isControllerOfType(nrpnTypeLSB)) {
                 nrpnTypeLSBvalue = midiMessage.getControllerValue();
+                incompleteNRPN.addEvent(midiMessage, (int)midiMessage.getTimeStamp());
                 nrpnTypeLSBvalueReceived = true;
             }
             if (midiMessage.isControllerOfType(nrpnValueMSB)) {
                 nrpnValueMSBvalue = midiMessage.getControllerValue();
+                incompleteNRPN.addEvent(midiMessage, (int)midiMessage.getTimeStamp());
                 nrpnValueMSBvalueReceived = true;
             }
             if (midiMessage.isControllerOfType(nrpnValueLSB)) {
                 nrpnValueLSBvalue = midiMessage.getControllerValue();
+                incompleteNRPN.addEvent(midiMessage, (int)midiMessage.getTimeStamp());
                 nrpnValueLSBvalueReceived = true;
             }
         }
-        if (nrpnTypeMSBvalueReceived && nrpnTypeLSBvalueReceived && nrpnValueMSBvalueReceived && nrpnValueLSBvalueReceived) {
-            auto newValue{ nrpnValueMSBvalue + nrpnValueLSBvalue };
-            auto nrpnType{ nrpnTypeMSBvalue + nrpnTypeLSBvalue };
-            applyIncomingNRPNvalueToExposedParameter(nrpnType, newValue);
-            nrpnTypeMSBvalueReceived = false;
-            nrpnTypeLSBvalueReceived = false;
-            nrpnValueMSBvalueReceived = false;
-            nrpnValueLSBvalueReceived = false;
+    }
+    if (nrpnTypeMSBvalueReceived && nrpnTypeLSBvalueReceived && nrpnValueMSBvalueReceived && nrpnValueLSBvalueReceived) {
+        auto newValue{ nrpnValueMSBvalue + nrpnValueLSBvalue };
+        auto nrpnType{ nrpnTypeMSBvalue + nrpnTypeLSBvalue };
+        applyIncomingNRPNvalueToExposedParameter(nrpnType, newValue);
+    }
+    else {
+        for (auto nrpnEvent : incompleteNRPN) {
+            midiMessagesToPassThrough.addEvent(nrpnEvent.getMessage(), (int)nrpnEvent.getMessage().getTimeStamp());
         }
     }
+    nrpnTypeMSBvalueReceived = false;
+    nrpnTypeLSBvalueReceived = false;
+    nrpnValueMSBvalueReceived = false;
+    nrpnValueLSBvalueReceived = false;
     return midiMessagesToPassThrough;
 }
 
