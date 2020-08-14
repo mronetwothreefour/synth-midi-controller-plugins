6f12252fb45cadcffabb022f6199f15037006c53
Desktop <socl02@mr1234.com>
2020-08-13 19:16:32 -0600

    created widget_ComboBoxWithExposedParamAttacher.h and .cpp and widget_ToggleButtonWithWithExposedParamAttacher.h and .cpp. Added interface and implementation code for classes ComboBoxWithExposedParamAttacher and ToggleButtonWithWithExposedParamAttacher.

---
+++ MophoGUI/MophoGUI/Source/widgets_ComboBox/widget_ComboBoxWithExposedParamAttacher.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <JuceHeader.h>
+
+#include "../tooltipSetters/tooltipSetter_ForExposedParamComboBoxes.h"
+
+using ComboBoxAttachment = AudioProcessorValueTreeState::ComboBoxAttachment;
+
+
+
+class ComboBoxWithExposedParamAttacher : public Component
+{
+protected:
+	ComboBox comboBox;
+	std::unique_ptr<ComboBoxAttachment> attachment;
+	uint8 param;
+	TooltipSetterForExposedParamComboBoxes tooltipSetter;
+
+public:
+	ComboBoxWithExposedParamAttacher() = delete;
+
+	ComboBoxWithExposedParamAttacher(uint8 param, UnexposedParameters* unexposedParams);
+	void attachToExposedParameter(AudioProcessorValueTreeState* exposedParams);
+	void deleteAttachment();
+	~ComboBoxWithExposedParamAttacher();
+
+private:
+	//==============================================================================
+	JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(ComboBoxWithExposedParamAttacher)
+};
