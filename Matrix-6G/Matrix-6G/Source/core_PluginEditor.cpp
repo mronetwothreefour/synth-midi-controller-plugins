52dc469334c5fcfc8a5edda9f4c60b88b2f39b30
Desktop <socl02@mr1234.com>
2020-09-20 19:42:15 -0600

    Matrix: Created gui_Colors.h and added color definitions. Set the size of the editor and changed the background to the device color.

--- Matrix-6G/Matrix-6G/Source/core_PluginEditor.cpp
+++ Matrix-6G/Matrix-6G/Source/core_PluginEditor.cpp
@@ -1,17 +1,19 @@
 #include "core_PluginProcessor.h"
 #include "core_PluginEditor.h"
 
+#include "gui/gui_Colors.h"
+
 
 
 PluginEditor::PluginEditor(PluginProcessor& processor) :
     AudioProcessorEditor{ &processor },
     processor{ processor }
 {
-    setSize(400, 300);
+    setSize(1256, 612);
 }
 
 void PluginEditor::paint(juce::Graphics& g) {
-    g.fillAll(getLookAndFeel().findColour(juce::ResizableWindow::backgroundColourId));
+    g.fillAll(Color::device);
     g.setColour(juce::Colours::white);
     g.setFont(15.0f);
     g.drawFittedText("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
---
+++ Matrix-6G/Matrix-6G/Source/gui/gui_Colors.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <JuceHeader.h>
+
+
+
+namespace Color
+{
+	static const Colour black{ 0xff000000 };
+	static const Colour button_blue{ 0xff05239d };
+	static const Colour button_gray{ 0xff3f3f3f };
+	static const Colour button_orange{ 0xffd74000 };
+	static const Colour button_red{ 0xff870000 };
+	static const Colour device{ 0xff161619 };
+	static const Colour dividerLine_gray{ 0xff5b5b5b };
+	static const Colour led_blue{ 0xff0096a6 };
+	static const Colour sectionDivider_blue{ 0xff05239d };
+	static const Colour white{ 0xffffffff };
+}
