985127ff87bb103ffed8ab329a794e3b0b2f37e4
Desktop <socl02@mr1234.com>
2020-09-24 11:05:23 -0600

    Matrix: Created widget_ControlTypes.h and added enum class ControlType. Created params_ExposedParamsInfo_Singleton.h.

---
+++ Matrix-6G/Matrix-6G/Source/widgets_ControlsForParameters/widget_ControlTypes.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <JuceHeader.h>
+
+
+
+enum class ControlType
+{
+	nullControl = -1,
+	slider,
+	sliderWithWaveShapeDisplay,
+	pgmSlotRadioButton,
+	button,
+	comboBox
+};
+
