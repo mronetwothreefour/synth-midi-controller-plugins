9b94ab98a1b2e85b73c4a51b3a954f20ebf18c18
Desktop <socl02@mr1234.com>
2020-09-30 19:49:28 -0600

    Matrix: refactored the paint() code in SectionHeadersLayer.h into three shorter functions and moved the implementation code into a .cpp file.

---
+++ Matrix-6G/Matrix-6G/Source/gui/gui_Layer_SectionHeaders.cpp
@@ -0,0 +1,40 @@
+#include "gui_Layer_SectionHeaders.h"
+
+#include "gui_Colors.h"
+#include "gui_InfoForSectionHeaders_Singleton.h"
+
+
+
+void SectionHeadersLayer::paint(Graphics& g) {
+	auto& info{ InfoForSectionHeaders::get() };
+	for (auto header = 0; header != info.headerOutOfRange(); ++header) {
+		paintBlueBarForHeader(g, header);
+	}
+}
+
+void SectionHeadersLayer::paintBlueBarForHeader(Graphics& g, int header) {
+	auto& info{ InfoForSectionHeaders::get() };
+	g.setColour(Color::dividerLine_blue);
+	Rectangle<int> blueBar{ info.startPointFor(header), info.endPointFor(header) };
+	g.fillRect(blueBar);
+	createHeaderTextLayout(g, info.textFor(header), blueBar);
+}
+
+void SectionHeadersLayer::createHeaderTextLayout(Graphics& g, String headerText, Rectangle<int> blueBar) {
+	AttributedString text;
+	text.setJustification(Justification::centred);
+	text.append(headerText, headerFont, Color::offWhite);
+	TextLayout layout;
+	layout.createLayout(text, 150.0f);
+	paintTextLayoutInBlueBar(g, layout, blueBar);
+}
+
+void SectionHeadersLayer::paintTextLayoutInBlueBar(Graphics& g, TextLayout layout, Rectangle<int> blueBar) {
+	auto textGap_x{ blueBar.getX() + textGapHorizontalOffset };
+	auto textGap_y{ blueBar.getY() + textGapVerticalOffset };
+	auto textGap_w{ layout.getWidth() + textGapWidthBuffer };
+	Rectangle<float> textGap{ textGap_x, textGap_y, textGap_w, textGap_h };
+	g.setColour(Color::device);
+	g.fillRect(textGap);
+	layout.draw(g, textGap);
+}
--- Matrix-6G/Matrix-6G/Source/gui/gui_Layer_SectionHeaders.h
+++ Matrix-6G/Matrix-6G/Source/gui/gui_Layer_SectionHeaders.h
@@ -2,46 +2,28 @@
 
 #include <JuceHeader.h>
 
-#include "gui_Colors.h"
-#include "gui/gui_Fonts.h"
-#include "gui_InfoForSectionHeaders_Singleton.h"
+#include "gui_Fonts.h"
 
 
 
 class SectionHeadersLayer :
 	public Component
 {
+	const float textGapHorizontalOffset{ 10.0f };
+	const float textGapVerticalOffset{ -1.0f };
+	const float textGap_h{ 18.0f };
+	const float textGapWidthBuffer{ 10.0f };
+	const Font headerFont{ FontsDB::family_HeavyText, FontsDB::style_ForHeavyText, FontsDB::size_ForSectionHeaderText };
+
 public:
 	SectionHeadersLayer() = default;
-
-	void paint(Graphics& g) override {
-		const auto textGapHorizontalOffset{ 10.0f };
-		const auto textGapVerticalOffset{ -1.0f };
-		const auto textGap_h{ 18.0f };
-		const auto textGapWidthBuffer{ 10.0f };
-		auto& info{ InfoForSectionHeaders::get() };
-		Font headerFont(FontsDB::family_HeavyText, FontsDB::style_ForHeavyText, FontsDB::size_ForSectionHeaderText);
-		for (auto header = 0; header != info.headerOutOfRange(); ++header) {
-			g.setColour(Color::dividerLine_blue);
-			Rectangle<int> blueBar{ info.startPointFor(header), info.endPointFor(header) };
-			g.fillRect(blueBar);
-
-			AttributedString text;
-			text.setJustification(Justification::centred);
-			text.append(info.textFor(header), headerFont, Color::offWhite);
-			TextLayout layout;
-			layout.createLayout(text, 150.0f);
-			auto textGap_x{ blueBar.getX() + textGapHorizontalOffset };
-			auto textGap_y{ blueBar.getY() + textGapVerticalOffset };
-			auto textGap_w{ layout.getWidth() + textGapWidthBuffer };
-			Rectangle<float> textGap{ textGap_x, textGap_y, textGap_w, textGap_h };
-			g.setColour(Color::device);
-			g.fillRect(textGap);
-			layout.draw(g, textGap);
-		}
-	}
+	void paint(Graphics& g) override;
 
 private:
+	void paintBlueBarForHeader(Graphics& g, int header);
+	void createHeaderTextLayout(Graphics& g, String headerText, Rectangle<int> blueBar);
+	void paintTextLayoutInBlueBar(Graphics& g, TextLayout layout, Rectangle<int> blueBar);
+
 	//==============================================================================
 	JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(SectionHeadersLayer)
 };
