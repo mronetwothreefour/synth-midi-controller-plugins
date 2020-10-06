61fd4b8891c280d3e8ec7217c31145fd6ee7b17f
Desktop <socl02@mr1234.com>
2020-10-06 08:21:37 -0600

    Matrix: Made led_blue and device colors a bit lighter. Enlarged the path for LED segment N slightly.

--- Matrix-6G/Matrix-6G/Source/gui/gui_Colors.h
+++ Matrix-6G/Matrix-6G/Source/gui/gui_Colors.h
@@ -11,9 +11,9 @@ namespace Color
 	static const Colour button_gray{ 0xff3f3f3f };
 	static const Colour button_orange{ 0xffd74000 };
 	static const Colour button_red{ 0xff870000 };
-	static const Colour device{ 0xff101013 };
+	static const Colour device{ 0xff131316 };
 	static const Colour dividerLine_gray{ 0xff656565 };
 	static const Colour dividerLine_blue{ 0xff05239d };
-	static const Colour led_blue{ 0xff0096a6 };
+	static const Colour led_blue{ 0xff00bed6 };
 	static const Colour offWhite{ 0xffe1e1e1 };
 }
