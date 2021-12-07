bb9097cacd60bcea2aac4cbba597f02e69995d02
Desktop <socl02@mr1234.com>
2021-12-07 12:26:13 -0700

    Mopho & Matrix Standalone: Created midi_Constants.h and copied code from plugin.

---
+++ MophoGUI/MophoGUI Standalone/MophoGUI Standalone/Source/midi/midi_Constants.h
@@ -0,0 +1,37 @@
+#pragma once
+
+
+
+namespace constants
+{
+    namespace MIDI
+    {
+        static const int controllerFlag{ 0xB0 };
+        static const int firstProgramDataByte{ 6 };
+        static const int firstUnusedProgramDataByte{ 236 };
+        static const int globalBalanceByte{ 24 };
+        static const int globalTransposeLSByte{ 4 };
+        static const int globalTransposeMSByte{ 5 };
+        static const int globalFineTuneLSByte{ 6 };
+        static const int globalFineTuneMSByte{ 7 };
+        static const int globalHardwareReceiveChannelLSByte{ 8 };
+        static const int globalHardwareReceiveChannelMSByte{ 9 };
+        static const int globalMidiClockTypeByte{ 10 };
+        static const int globalMidiControllersOnByte{ 16 };
+        static const int globalParameterSendTypetByte{ 12 };
+        static const int globalParameterReceiveTypeByte{ 14 };
+        static const int globalPedalModeIsArpByte{ 26 };
+        static const int globalProgramChangeOnByte{ 28 };
+        static const int globalSysExOnByte{ 18 };
+        static const int globalStereoOutByte{ 20 };
+        static const int nrpnTypeForGlobalFineTune{ 385 };
+        static const int nrpnTypeForGlobalTranspose{ 384 };
+        static const int nrpnTypeMSB{ 99 };
+        static const int nrpnTypeLSB{ 98 };
+        static const int nrpnValueMSB{ 6 };
+        static const int nrpnValueLSB{ 38 };
+        static const int programDumpBankByte{ 4 };
+        static const int programDumpSlotByte{ 5 };
+        static const int sysExMessageTypeByte{ 3 };
+    }
+}
