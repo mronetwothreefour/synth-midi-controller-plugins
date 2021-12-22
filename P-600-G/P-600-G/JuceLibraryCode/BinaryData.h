c40291bd4d650b3d31bfcb72e98f44ac6fdb5369
Desktop <socl02@mr1234.com>
2021-12-22 12:44:17 -0700

    P-600-G: Created new plugin solution using Projucer.

---
+++ P-600-G/P-600-G/JuceLibraryCode/BinaryData.h
@@ -0,0 +1,69 @@
+/* =========================================================================================
+
+   This is an auto-generated file: Any edits you make may be overwritten!
+
+*/
+
+#pragma once
+
+namespace BinaryData
+{
+    extern const char*   ButtonPgmBankDown_png;
+    const int            ButtonPgmBankDown_pngSize = 984;
+
+    extern const char*   ButtonPgmBankUp_png;
+    const int            ButtonPgmBankUp_pngSize = 1005;
+
+    extern const char*   ButtonPullDown_png;
+    const int            ButtonPullDown_pngSize = 597;
+
+    extern const char*   ButtonPullUp_png;
+    const int            ButtonPullUp_pngSize = 606;
+
+    extern const char*   ButtonPushDown_png;
+    const int            ButtonPushDown_pngSize = 668;
+
+    extern const char*   ButtonPushUp_png;
+    const int            ButtonPushUp_pngSize = 697;
+
+    extern const char*   ButtonRandDown_png;
+    const int            ButtonRandDown_pngSize = 731;
+
+    extern const char*   ButtonRandUp_png;
+    const int            ButtonRandUp_pngSize = 721;
+
+    extern const char*   ButtonRedoDown_png;
+    const int            ButtonRedoDown_pngSize = 682;
+
+    extern const char*   ButtonRedoUp_png;
+    const int            ButtonRedoUp_pngSize = 689;
+
+    extern const char*   ButtonUndoDown_png;
+    const int            ButtonUndoDown_pngSize = 688;
+
+    extern const char*   ButtonUndoUp_png;
+    const int            ButtonUndoUp_pngSize = 691;
+
+    extern const char*   P600GMainWindowBackground_png;
+    const int            P600GMainWindowBackground_pngSize = 1136026;
+
+    extern const char*   Switch_png;
+    const int            Switch_pngSize = 139;
+
+    // Number of elements in the namedResourceList and originalFileNames arrays.
+    const int namedResourceListSize = 14;
+
+    // Points to the start of a list of resource names.
+    extern const char* namedResourceList[];
+
+    // Points to the start of a list of resource filenames.
+    extern const char* originalFilenames[];
+
+    // If you provide the name of one of the binary resource variables above, this function will
+    // return the corresponding data and its size (or a null pointer if the name isn't found).
+    const char* getNamedResource (const char* resourceNameUTF8, int& dataSizeInBytes);
+
+    // If you provide the name of one of the binary resource variables above, this function will
+    // return the corresponding original, non-mangled filename (or a null pointer if the name isn't found).
+    const char* getNamedResourceOriginalFilename (const char* resourceNameUTF8);
+}
---
+++ P-600-G/P-600-G/Source/PluginEditor.h
@@ -0,0 +1,33 @@
+/*
+  ==============================================================================
+
+    This file contains the basic framework code for a JUCE plugin editor.
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include <JuceHeader.h>
+#include "PluginProcessor.h"
+
+//==============================================================================
+/**
+*/
+class P600GAudioProcessorEditor  : public juce::AudioProcessorEditor
+{
+public:
+    P600GAudioProcessorEditor (P600GAudioProcessor&);
+    ~P600GAudioProcessorEditor() override;
+
+    //==============================================================================
+    void paint (juce::Graphics&) override;
+    void resized() override;
+
+private:
+    // This reference is provided as a quick way for your editor to
+    // access the processor object that created it.
+    P600GAudioProcessor& audioProcessor;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (P600GAudioProcessorEditor)
+};
