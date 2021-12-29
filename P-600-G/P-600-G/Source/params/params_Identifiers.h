02f1d02b96b88ba9ac795ae96ead1d324f0288b6
Desktop <socl02@mr1234.com>
2021-12-29 12:23:13 -0700

    P-600-G: Added Identifiers relating to TooltipOptions. Created params_TooltipOptions.h and added interface and implementation code for class TooltipOptions.

--- P-600-G/P-600-G/Source/params/params_Identifiers.h
+++ P-600-G/P-600-G/Source/params/params_Identifiers.h
@@ -6,5 +6,10 @@
 
 namespace ID
 {
+	static const Identifier state_TooltipOptions{ "state_TooltipOptions" };
 
+	static const Identifier tooltips_DelayInMilliseconds{ "tooltips_DelayInMilliseconds" };
+	static const Identifier tooltips_Options{ "tooltips_Options" };
+	static const Identifier tooltips_ShouldShowCurrentValue{ "tooltips_ShouldShowCurrentValue" };
+	static const Identifier tooltips_ShouldShowDescription{ "tooltips_ShouldShowDescription" };
 }
