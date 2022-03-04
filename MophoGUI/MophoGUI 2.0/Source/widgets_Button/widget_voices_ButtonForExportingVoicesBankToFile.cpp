15d4b1ec35132df7ef056f14246b6f0a0070e944
Desktop <socl02@mr1234.com>
2022-03-04 09:40:30 -0700

    Mopho 2: Created widget_voices_ButtonForExportingVoicesBankToFile.h and .cpp and added interface and implementation code for class ButtonForExportingVoicesBankToFile.

---
+++ MophoGUI/MophoGUI 2.0/Source/widgets_Button/widget_voices_ButtonForExportingVoicesBankToFile.cpp
@@ -0,0 +1,26 @@
+#include "widget_voices_ButtonForExportingVoicesBankToFile.h"
+
+#include "../params/params_Identifiers.h"
+#include "../params/params_UnexposedParameters_Facade.h"
+
+
+
+ButtonForExportingVoicesBankToFile::ButtonForExportingVoicesBankToFile(UnexposedParameters* unexposedParams) :
+	BaseButtonWithOnClickAndTooltipMethods{ unexposedParams },
+	unexposedParams{ unexposedParams }
+{
+	setComponentID(ID::button_Export.toString());
+	setTooltip(createButtonTooltipString());
+}
+
+const String ButtonForExportingVoicesBankToFile::createButtonTooltipString() {
+	String buttonTooltip{ "" };
+	if (unexposedParams->tooltipOptions_get()->shouldShowDescriptions()) {
+		buttonTooltip += "Exports the data for all the programs in the storage bank to a\n";
+		buttonTooltip += "file which can be read by other instances of the MophoGUI plugin.";
+	}
+	return buttonTooltip;
+}
+
+void ButtonForExportingVoicesBankToFile::onClickMethod() {
+}
