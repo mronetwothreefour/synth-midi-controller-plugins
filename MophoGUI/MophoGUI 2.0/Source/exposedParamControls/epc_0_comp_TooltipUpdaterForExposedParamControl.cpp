2c08afb3eb40e061ce3311cccd787be7897dbab0
Desktop <socl02@mr1234.com>
2022-07-20 10:15:34 -0600

    Mopho 2: Added back code for TooltipUpdaterForExposedParamControl. Made it a Value::Listener instead of an AudioProcessorValueTreeState::Listener and ValueTree::Listener; added Values parameterValue, shouldShowCurrentChoiceValue, and shouldShowDescriptionValue; declared and defined valueChanged(), which updates the tooltip whenever one of the listened-to Values changes.

---
+++ MophoGUI/MophoGUI 2.0/Source/exposedParamControls/epc_0_comp_TooltipUpdaterForExposedParamControl.cpp
@@ -0,0 +1,55 @@
+#include "epc_0_comp_TooltipUpdaterForExposedParamControl.h"
+
+#include "../exposedParameters/ep_3_facade_ExposedParameters.h"
+#include "../unexposedParameters/up_1_facade_UnexposedParameters.h"
+
+TooltipUpdaterForExposedParamControl::TooltipUpdaterForExposedParamControl(
+    const uint8 paramIndex, SettableTooltipClient& clientComponent, ExposedParameters* exposedParams, UnexposedParameters* unexposedParams) :
+    paramIndex{ paramIndex },
+    clientComponent{ clientComponent },
+    state{ exposedParams->state.get() },
+    info{ exposedParams->info.get() }
+{
+    auto paramID{ info->IDfor(paramIndex) };
+    parameterValue = state->getParameterAsValue(paramID);
+    parameterValue.addListener(this);
+
+    shouldShowDescriptionValue = unexposedParams->getTooltipsOptions()->getShouldShowDescriptionValue();
+    shouldShowDescriptionValue.addListener(this);
+
+    shouldShowCurrentChoiceValue = unexposedParams->getTooltipsOptions()->getShouldShowCurrentChoiceValue();
+    shouldShowCurrentChoiceValue.addListener(this);
+
+    updateTooltip();
+}
+
+void TooltipUpdaterForExposedParamControl::updateTooltip(){
+    auto tip{ generateTooltipText() };
+    clientComponent.setTooltip(tip);
+}
+
+String TooltipUpdaterForExposedParamControl::generateTooltipText() {
+    auto shouldShowDescription{ (bool)shouldShowDescriptionValue.getValue() };
+    auto shouldShowCurrentChoice{ (bool)shouldShowCurrentChoiceValue.getValue() };
+    String tip{ "" };
+    if (shouldShowDescription)
+        tip += info->descriptionFor(paramIndex) + "\n";
+    if (shouldShowCurrentChoice) {
+        auto paramID{ info->IDfor(paramIndex) };
+        auto paramaterPtr{ state->getParameter(paramID) };
+        auto currentChoice{ roundToInt(paramaterPtr->convertFrom0to1(paramaterPtr->getValue())) };
+        auto choiceName{ info->verboseChoiceNameFor((uint8)currentChoice, paramIndex) };
+        tip += "Current Setting: " + choiceName;
+    }
+    return tip;
+}
+
+void TooltipUpdaterForExposedParamControl::valueChanged(Value& /*value*/) {
+    updateTooltip();
+}
+
+TooltipUpdaterForExposedParamControl::~TooltipUpdaterForExposedParamControl() {
+    parameterValue.removeListener(this);
+    shouldShowDescriptionValue.removeListener   (this);
+    shouldShowCurrentChoiceValue.removeListener(this);
+}
---
+++ MophoGUI/MophoGUI 2.0/Source/exposedParamControls/epc_0_comp_TooltipUpdaterForExposedParamControl.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <JuceHeader.h>
+
+class ExposedParameters;
+class InfoForExposedParameters;
+class TooltipsOptions;
+class UnexposedParameters;
+
+class TooltipUpdaterForExposedParamControl :
+	public Value::Listener
+{
+	uint8 paramIndex;
+	SettableTooltipClient& clientComponent;
+	AudioProcessorValueTreeState* state;
+	InfoForExposedParameters* info;
+	Value parameterValue;
+	Value shouldShowDescriptionValue;
+	Value shouldShowCurrentChoiceValue;
+
+public:
+	TooltipUpdaterForExposedParamControl() = delete;
+
+	TooltipUpdaterForExposedParamControl(const uint8 paramIndex, SettableTooltipClient& clientComponent, ExposedParameters* exposedParams, UnexposedParameters* unexposedParams);
+
+private:
+	void updateTooltip();
+	String generateTooltipText();
+
+public:
+	void valueChanged(Value& value) override;
+	~TooltipUpdaterForExposedParamControl();
+
+private:
+	//==============================================================================
+	JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(TooltipUpdaterForExposedParamControl)
+};
