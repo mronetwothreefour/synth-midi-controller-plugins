ace54073b7795b4268449e1911d88ce2781bacc1
mronetwothreefour <41154573+mronetwothreefour@users.noreply.github.com>
2026-05-04 08:07:20 -0600

    MiniloGUI: Initialized Juce project.

---
+++ MiniloGUI/MiniloGUI 2026/Source/gui/G _0_Editor.cpp
@@ -0,0 +1,27 @@
+#include "G _0_Editor.h"
+
+Editor::Editor(Processor& processor) :
+    AudioProcessorEditor{ &processor },
+    processor{ processor }
+{
+    setWantsKeyboardFocus(true);
+    setResizable(true, true);
+    setSize (400, 300);
+    Timer::callAfterDelay(50, [this] { grabKeyboardFocus(); });
+}
+
+void Editor::paint (Graphics& g) {
+    g.fillAll (getLookAndFeel().findColour (ResizableWindow::backgroundColourId));
+    g.setColour (Colours::white);
+    g.setFont (FontOptions (15.0f));
+    g.drawFittedText ("Hello World!", getLocalBounds(), Justification::centred, 1);
+}
+
+void Editor::resized() {
+}
+
+void Editor::modifierKeysChanged(const ModifierKeys& /*mods*/) {
+}
+
+Editor::~Editor() {
+}
