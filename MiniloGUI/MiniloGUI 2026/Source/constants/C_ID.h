db55ae9eb50fa6f5a3edd6ddc53c57d4f0cedecd
mronetwothreefour <41154573+mronetwothreefour@users.noreply.github.com>
2026-05-05 17:22:28 -0600

    MiniloGUI: Exposed parameter Identifiers

---
+++ MiniloGUI/MiniloGUI 2026/Source/constants/C_ID.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include <JuceHeader.h>
+
+namespace ID
+{
+
+	static const Identifier ep_00_osc_1_octave       { "ep_00_osc_1_octave" };
+	static const Identifier ep_01_osc_1_wave         { "ep_01_osc_1_wave" };
+	static const Identifier ep_02_osc_1_pitch        { "ep_02_osc_1_pitch" };
+	static const Identifier ep_03_osc_1_shape        { "ep_03_osc_1_shape" };
+	static const Identifier ep_04_osc_2_octave       { "ep_04_osc_2_octave" };
+	static const Identifier ep_05_osc_2_wave         { "ep_05_osc_2_wave" };
+	static const Identifier ep_06_osc_2_pitch        { "ep_06_osc_2_pitch" };
+	static const Identifier ep_07_osc_2_shape        { "ep_07_osc_2_shape" };
+	static const Identifier ep_08_osc_2_xmod_depth   { "ep_08_osc_2_xmod_depth" };
+	static const Identifier ep_09_osc_2_pitch_eg_int { "ep_09_osc_2_pitch_eg_int" };
+	static const Identifier ep_10_osc_2_sync         { "ep_10_osc_2_sync" };
+	static const Identifier ep_11_osc_2_ringMod      { "ep_11_osc_2_ringMod" };
+	static const Identifier ep_12_osc_1_level        { "ep_12_osc_1_level" };
+	static const Identifier ep_13_osc_2_level        { "ep_13_osc_2_level" };
+	static const Identifier ep_14_noise_level        { "ep_14_noise_level" };
+	static const Identifier ep_15_lpf_freq           { "ep_15_lpf_freq" };
+	static const Identifier ep_16_lpf_reso           { "ep_16_lpf_reso" };
+	static const Identifier ep_17_lpf_eg_int         { "ep_17_lpf_eg_int" };
+	static const Identifier ep_18_lpf_type           { "ep_18_lpf_type" };
+	static const Identifier ep_19_lpf_key_track      { "ep_19_lpf_key_track" };
+	static const Identifier ep_20_lpf_vel_amt        { "ep_20_lpf_vel_amt" };
+	static const Identifier ep_21_vca_env_attack     { "ep_21_vca_env_attack" };
+	static const Identifier ep_22_vca_env_decay      { "ep_22_vca_env_decay" };
+	static const Identifier ep_23_vca_env_sustain    { "ep_23_vca_env_sustain" };
+	static const Identifier ep_24_vca_env_release    { "ep_24_vca_env_release" };
+	static const Identifier ep_25_env_attack         { "ep_25_env_attack" };
+	static const Identifier ep_26_env_decay          { "ep_26_env_decay" };
+	static const Identifier ep_27_env_sustain        { "ep_27_env_sustain" };
+	static const Identifier ep_28_env_release        { "ep_28_env_release" };
+	static const Identifier ep_29_LFO_Wave           { "ep_29_LFO_Wave" };
+	static const Identifier ep_30_lfo_eg_mod         { "ep_30_lfo_eg_mod" };
+	static const Identifier ep_31_lfo_rate           { "ep_31_lfo_rate" };
+	static const Identifier ep_32_lfo_int            { "ep_32_lfo_int" };
+	static const Identifier ep_33_lfo_target         { "ep_33_lfo_target" };
+	static const Identifier ep_34_delay_hpf_freq     { "ep_34_delay_hpf_freq" };
+	static const Identifier ep_35_delay_time         { "ep_35_delay_time" };
+	static const Identifier ep_36_delay_feedback     { "ep_36_delay_feedback" };
+	static const Identifier ep_37_delay_routing      { "ep_37_delay_routing" };
+	static const Identifier ep_38_voice_mode_depth   { "ep_38_voice_mode_depth" };
+
+}
