484dea683f68ee0207db694827ff3eacb52b4242
mronetwothreefour <41154573+mronetwothreefour@users.noreply.github.com>
2026-06-20 19:09:37 -0600

    Matrix: Exposed control coordinates
    Remembered that the x and y coordinates are for the control center, not the top-left corner, so I had to roll back the XYWH changes I made 5 days ago.

--- Matrix-6G/Matrix-6G 2026/Project_Source_Files/D_X_G_Editor_P.cpp
+++ Matrix-6G/Matrix-6G 2026/Project_Source_Files/D_X_G_Editor_P.cpp
@@ -6,17 +6,17 @@ using namespace XYWH;
 
 Editor_P::Editor_P(Audio_Processor_P& processor, Data_Hub_P* hub) :
     Editor_B{ processor, hub },
-    knob{ new Knob_Exposed_B{ 0, hub } }
+    knob{ new Knob_Exposed_B{ 2, hub } }
 {
     knob->attach_to_param();
-    knob->set_modifying_pitch(true);
     addAndMakeVisible(knob.get());
     resized();
 }
 
 void Editor_P::resized() {
     scale_factor = (float)getWidth() / XYWH::gui_init_w;
-    Rectangle<int> knob_bounds{ osc_col_1_x, ctrl_row_01_y, osc_ctrl_w, ctrl_h };
+    Rectangle<int> knob_bounds{ 0, 0, osc_ctrl_w, ctrl_h };
+    knob_bounds.setCentre(exp_info.ctrl_center_for(3));
     knob->setBounds(knob_bounds.transformedBy(AffineTransform::scale(scale_factor)));
 }
 
--- Matrix-6G/Matrix-6G 2026/Project_Source_Files/c_XYWH_p.h
+++ Matrix-6G/Matrix-6G 2026/Project_Source_Files/c_XYWH_p.h
@@ -35,7 +35,7 @@ namespace XYWH
 
 	constexpr int ctrl_h{ 20 };
 	constexpr int ctrl_row_h{ ctrl_h + 8 };
-	constexpr int ctrl_row_01_y{ 59 };
+	constexpr int ctrl_row_01_y{ 69 };
 	constexpr int ctrl_row_02_y{ ctrl_row_01_y + ctrl_row_h };
 	constexpr int ctrl_row_03_y{ ctrl_row_02_y + ctrl_row_h };
 	constexpr int ctrl_row_04_y{ ctrl_row_03_y + ctrl_row_h };
@@ -50,13 +50,13 @@ namespace XYWH
 
 	constexpr int env_block_spacing{ 410 };
 	constexpr int env_dadsr_spacing{ 50 };
-	constexpr int env_col_1_x{ 98 };
-	constexpr int env_1_delay_x{ 184 };
+	constexpr int env_col_1_x{ 134 };
+	constexpr int env_1_delay_x{ 198 };
 	constexpr int env_1_attack_x{ env_1_delay_x + env_dadsr_spacing };
 	constexpr int env_1_decay_x{ env_1_attack_x + env_dadsr_spacing };
 	constexpr int env_1_sustain_x{ env_1_decay_x + env_dadsr_spacing };
 	constexpr int env_1_release_x{ env_1_sustain_x + env_dadsr_spacing };
-	constexpr int env_ctrl_row_1_y{ 440 };
+	constexpr int env_ctrl_row_1_y{ 450 };
 	constexpr int env_ctrl_row_2_y{ env_ctrl_row_1_y + ctrl_row_h };
 	constexpr int env_ctrl_row_3_y{ env_ctrl_row_2_y + ctrl_row_h };
 	constexpr int env_ctrl_row_4_y{ env_ctrl_row_3_y + ctrl_row_h };
@@ -66,12 +66,12 @@ namespace XYWH
 
 	constexpr int gui_init_w{ 1252 }, gui_init_h{ 596 };
 
-	constexpr int osc_col_1_x{ 100 };
-	constexpr int osc_col_2_x{ 166 };
+	constexpr int osc_col_1_x{ 130 };
+	constexpr int osc_col_2_x{ 196 };
 	constexpr int osc_ctrl_w{ 60 };
 
-	constexpr int lfo_col_1_x{ 696 };
-	constexpr int lfo_col_2_x{ 762 };
+	constexpr int lfo_col_1_x{ 726 };
+	constexpr int lfo_col_2_x{ 792 };
 	constexpr int lfo_ctrl_w{ 60 };
 
 	constexpr int mmod_col_1_x{ 1016 };
@@ -89,25 +89,25 @@ namespace XYWH
 	constexpr int patch_num_x{ 840 };
 	constexpr int patch_num_w{ 28 };
 
-	constexpr int porta_key_mode_col_x{ 508 };
+	constexpr int porta_key_mode_col_x{ 544 };
 	constexpr int porta_key_mode_col_w{ 72 };
 
-	constexpr int ramp_col_1_x{ 864 };
-	constexpr int ramp_col_2_x{ 930 };
+	constexpr int ramp_col_1_x{ 894 };
+	constexpr int ramp_col_2_x{ 960 };
 	constexpr int ramp_ctrl_w{ 60 };
 
-	constexpr int track_input_x{ 896 };
-	constexpr int track_input_y{ 155 };
+	constexpr int track_input_x{ 943 };
+	constexpr int track_input_y{ 165 };
 	constexpr int track_input_w{ 94 };
 	constexpr int track_pt_spacing{ 31 };
-	constexpr int track_pt_1_x{ 840 };
+	constexpr int track_pt_1_x{ 853 };
 	constexpr int track_pt_w{ 26 };
 	constexpr int track_pt_y{ 279 };
 
-	constexpr int vcf_fm_col_x{ 520 };
+	constexpr int vcf_fm_col_x{ 550 };
 	constexpr int vcf_fm_ctrl_w{ 60 };
 
-	constexpr int vcf_vca_col_x{ 352 };
+	constexpr int vcf_vca_col_x{ 382 };
 	constexpr int vcf_vca_ctrl_w{ 60 };
 
 }
