544bc8c676b8d1af3aaac93d0cc4961086411f1a
mronetwothreefour <41154573+mronetwothreefour@users.noreply.github.com>
2026-07-18 07:28:42 -0600

    All: C_COLOR

---
+++ MophoGUI/MophoGUI 2026/Project_Source/C_COLOR_P.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <JuceHeader.h>
+
+#include "C_COLOR_G.h"
+
+namespace COLOR
+{
+
+	const Colour ground{ 0xffde9600 };
+	const Colour red_btn{ 0xff870000 };
+	const Colour red_btn_lite_1{ 0xffb40000 };
+	const Colour red_btn_lite_2{ 0xffc80000 };
+	const Colour red_btn_dark_1{ 0xff500000 };
+	const Colour red_btn_dark_2{ 0xff640000 };
+	const Colour red_toggle_off{ 0xff4a0000 };
+	const Colour red_toggle_on{ 0xffff0000 };
+	const Colour tip_border{ COLOR::white };
+	const Colour txt_caret{ yellow.withAlpha(0.9f) };
+	const Colour txt_highlight{ yellow.withAlpha(0.6f) };
+	const Colour yellow{ 0xffffbe00 };
+
+}
---
+++ _Global_Source/C_COLOR_G.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <JuceHeader.h>
+
+namespace COLOR
+{
+
+	const Colour black{ 0xff000000 };
+	const Colour popup_bkgrnd{ 0xff18181a };
+	const Colour text{ 0xffffffff };
+	const Colour white{ 0xffffffff };
+
+}
