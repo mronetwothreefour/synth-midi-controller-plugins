e9d747b611fce58b5973871ef05684bf7cd7e0fd
mronetwothreefour <41154573+mronetwothreefour@users.noreply.github.com>
2026-07-22 06:53:34 -0600

    Mini: Tree_Exposed_Info_P

---
+++ MiniloGUI/MiniloGUI 2026/Project_Source/D_TREE_Exposed_Info_P.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <JuceHeader.h>
+
+#include "D_TREE_Exposed_Info_G.h"
+
+class Tree_Exposed_Info_P :
+	public Tree_Exposed_Info_G
+{
+//==============================================================================
+public: Tree_Exposed_Info_P();
+
+public: const int cc_num_for(const Exp_Param id) const;
+public: const int bit_count_for(const Exp_Param id) const;
+public: const int byte_index_for_param_bit(const Exp_Param id, const int bit_index) const;
+public: const int bit_index_for_param_bit(const Exp_Param id, const int bit_index) const;
+
+//==============================================================================
+private: JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(Tree_Exposed_Info_P)
+};
