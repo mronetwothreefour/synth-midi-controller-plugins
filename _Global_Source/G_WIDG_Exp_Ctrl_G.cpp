f7c762ca312f6e9bdbcba2874dfb21d087014f1c
mronetwothreefour <41154573+mronetwothreefour@users.noreply.github.com>
2026-07-25 07:21:55 -0600

    All: Exposed_Control_G

---
+++ _Global_Source/G_WIDG_Exp_Ctrl_G.cpp
@@ -0,0 +1,37 @@
+#include "G_WIDG_Exp_Ctrl_G.h"
+
+Exposed_Control_G::Exposed_Control_G(const Exp_Param param_id, Data_Hub_P* hub) :
+	Data_User_P{ hub },
+	param_id{ param_id },
+	exp_param{ exp_state->getParameter(String{ param_id })},
+	val_exp_param{ exp_state->getParameterAsValue(String{ param_id }) },
+	mod_param{ nullptr },
+	ctrl_type{ exp_info.ctrl_type_for(param_id) }
+{
+	val_exp_param.addListener(this);
+	init_bounds.setSize(exp_info.ctrl_width_for(param_id),
+						exp_info.ctrl_height_for(param_id));
+	init_bounds.setCentre(exp_info.ctrl_center_for(param_id));
+	tip_update.tip_info = exp_info.tip_for(param_id);
+	update_value_tip();
+}
+
+Rectangle<int> Exposed_Control_G::get_scaled_bounds() {
+	return init_bounds * scale_factor;
+}
+
+void Exposed_Control_G::update_value_tip() {
+	auto choice_num = roundToInt(exp_param->convertFrom0to1(exp_param->getValue()));
+	tip_update.tip_value = exp_info.choice_for(param_id, choice_num);
+}
+
+void Exposed_Control_G::valueChanged(Value& v) {
+	if (v.refersToSameSourceAs(val_exp_param))
+		update_value_tip();
+	if (v.refersToSameSourceAs(val_mod_param))
+		update_according_to_mod();
+}
+
+Exposed_Control_G::~Exposed_Control_G() {
+	val_exp_param.removeListener(this);
+}
---
+++ _Global_Source/G_WIDG_Exp_Ctrl_G.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "D_X_G_Data_User_P.h"
+#include "G_WIDG_Tip_Updater_G.h"
+
+class Exposed_Control_G:
+	public Data_User_P,
+	public Value::Listener
+{
+protected: const Exp_Param param_id;
+protected: RangedAudioParameter* exp_param;
+protected: Value val_exp_param;
+protected: int mod_param_index;
+protected: RangedAudioParameter* mod_param;
+protected: Value val_mod_param;
+public: const Ctrl_Type ctrl_type;
+private: Rectangle<int> init_bounds;
+protected: Tip_Updater_G tip_update;
+
+//==============================================================================
+public: Exposed_Control_G(const Exp_Param param_id, Data_Hub_P* hub);
+
+public: virtual void attach_to_param()=0;
+public: virtual void remove_attachment()=0;
+public: Rectangle<int> get_scaled_bounds();
+public: virtual void update_value_tip();
+public: virtual void update_according_to_mod() {};
+public: void valueChanged(Value& value) override;
+public: ~Exposed_Control_G();
+
+//==============================================================================
+private: JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(Exposed_Control_G)
+};
