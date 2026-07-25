063b6f9836edde98f1a01a0cedf68c0b0fa50780
mronetwothreefour <41154573+mronetwothreefour@users.noreply.github.com>
2026-07-25 07:58:17 -0600

    All: Combo_Box_Exposed_G.

---
+++ _Global_Source/G_WIDG_Cbox_Exp_G.cpp
@@ -0,0 +1,28 @@
+#include "G_WIDG_Cbox_Exp_G.h"
+
+Combo_Box_Exposed_G::Combo_Box_Exposed_G(const Exp_Param param_id, Data_Hub_P* hub) :
+	Exposed_Control_G{ param_id, hub }
+{
+	auto choices_list = exp_info.choices_list_for(param_id, true);
+	addItemList(choices_list, 1);
+}
+
+inline void Combo_Box_Exposed_G::attach_to_param() {
+	attachment.reset(new Cbox_Attachment{ *exp_param, *this, Exposed_Control_G::u_m });
+}
+
+inline void Combo_Box_Exposed_G::remove_attachment() {
+	attachment = nullptr;
+}
+
+inline void Combo_Box_Exposed_G::mouseDown(const MouseEvent& e) {
+	if (isPopupActive() && e.mods == ModifierKeys::leftButtonModifier) {
+		hidePopup();
+		return;
+	}
+	ComboBox::mouseDown(e);
+}
+
+inline void Combo_Box_Exposed_G::modifierKeysChanged(const Mods& mods) {
+	tip_update.on_mod_keys_changed(mods, this);
+}
---
+++ _Global_Source/G_WIDG_Cbox_Exp_G.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <JuceHeader.h>
+
+#include "G_WIDG_Exp_Ctrl_G.h"
+
+using Cbox_Attachment = ComboBoxParameterAttachment;
+
+class Combo_Box_Exposed_G :
+	public Exposed_Control_G,
+	public ComboBox
+{
+protected: std::unique_ptr<Cbox_Attachment> attachment;
+
+//==============================================================================
+public: Combo_Box_Exposed_G(const Exp_Param param_id, Data_Hub_P* hub);
+
+public: void attach_to_param() override;
+public: void remove_attachment() override;
+public: void mouseDown(const MouseEvent& e) override;
+public: void modifierKeysChanged(const Mods& mods) override;
+
+//==============================================================================
+private: JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(Combo_Box_Exposed_G)
+};
