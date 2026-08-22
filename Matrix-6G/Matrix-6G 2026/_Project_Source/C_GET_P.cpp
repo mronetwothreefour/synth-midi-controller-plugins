d0853426039cd524e42ce2429636d94eb4adf12f
mronetwothreefour <41154573+mronetwothreefour@users.noreply.github.com>
2026-08-22 08:03:36 -0600

    Matrix: MAP::byte_index, GET::byte_index_for.

--- Matrix-6G/Matrix-6G 2026/_Project_Source/C_GET_P.cpp
+++ Matrix-6G/Matrix-6G 2026/_Project_Source/C_GET_P.cpp
@@ -1,6 +1,13 @@
 #include "C_GET_P.h"
 
+#include "C_MAP_Byte_Index_P.h"
 #include "C_MAP_Quick_Num_P.h"
+
+const int GET::byte_index_for(const String& id) {
+	if (MAP::byte_index.find(id) != MAP::byte_index.end())
+		return MAP::byte_index.at(id);
+	return -1;
+}
 
 const int GET::quick_num_for(const String& id) {
 	if (MAP::quick_num.find(id) != MAP::quick_num.end())
--- Matrix-6G/Matrix-6G 2026/_Project_Source/C_GET_P.h
+++ Matrix-6G/Matrix-6G 2026/_Project_Source/C_GET_P.h
@@ -5,5 +5,6 @@
 struct GET :
 	public GET_A
 {
+	static const int byte_index_for(const String& id);
 	static const int quick_num_for(const String& id);
 };
