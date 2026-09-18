287ca5d0361cb0352880e7617e926b73f91bb686
mronetwothreefour <41154573+mronetwothreefour@users.noreply.github.com>
2026-09-18 07:45:52 -0600

    Matrix: txt_editor_traits
    Defined static const tuples for the duplicate traits.

--- Matrix-6G/Matrix-6G 2026/_Project_Source/C_MISC_P.h
+++ Matrix-6G/Matrix-6G 2026/_Project_Source/C_MISC_P.h
@@ -1,7 +1,5 @@
 #pragma once
 
-#include <JuceHeader.h>
-
 #include "C_MISC_A.h"
 
 namespace MISC
@@ -12,5 +10,10 @@ namespace MISC
 	constexpr auto knb_angle_1 = degreesToRadians(495.0f);
 	constexpr auto mod_dst_count = 33;
 	constexpr auto mod_src_count = 21;
+
+	static const tuple<string, int, string>
+		txt_editor_traits_osc_pitch{ allowed_chars_pitch, 3, Tip_For::txt_editor_pitch(25, 0, 63) },
+		txt_editor_traits_s_6_bit{ allowed_chars_s_int, 3, Tip_For::txt_editor_s_int(63) },
+		txt_editor_traits_u_6_bit{ allowed_chars_u_int, 2, Tip_For::txt_editor_u_int(63) };
 
 }
--- _Abstract_Source/C_MISC_A.h
+++ _Abstract_Source/C_MISC_A.h
@@ -1,11 +1,23 @@
 #pragma once
 
+#include <tuple>
+
+#include "D_BUILD_Tip_For_P.h"
+
+using namespace BUILD;
+using namespace std;
+
 namespace MISC
 {
 
-	static const auto allowed_chars_pitch{ "abcdefgABCDEFG0123456789#" };
-	static const auto allowed_chars_s_int{ "-0123456789" };
-	static const auto allowed_chars_u_int{ "0123456789" };
-	static const auto pitch_name_chars{ "abcdefgABCDEFG#" };
+	constexpr auto 
+		allowed_chars_pitch{ "abcdefgABCDEFG0123456789#" },
+		allowed_chars_s_int{ "-0123456789" },
+		allowed_chars_u_int{ "0123456789" },
+		pitch_name_chars{ "abcdefgABCDEFG#" };
+
+	static const tuple<string, int, string> 
+		txt_editor_traits_s_7_bit{ allowed_chars_s_int, 4, Tip_For::txt_editor_s_int(127) },
+		txt_editor_traits_u_7_bit{ allowed_chars_u_int, 3, Tip_For::txt_editor_u_int(127) };
 
 }
