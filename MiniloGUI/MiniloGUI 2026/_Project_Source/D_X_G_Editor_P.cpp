2136fee2b3e97d39fca3ed947e46565ee3c22ada
mronetwothreefour <41154573+mronetwothreefour@users.noreply.github.com>
2026-09-24 11:41:42 -0600

    P-600 & Mini: Audio_Processor, Editor.

--- MiniloGUI/MiniloGUI 2026/_Project_Source/D_X_G_Editor_P.cpp
+++ MiniloGUI/MiniloGUI 2026/_Project_Source/D_X_G_Editor_P.cpp
@@ -3,24 +3,26 @@
 #include "C_ID_A.h"
 #include "C_GET_P.h"
 
-using namespace ID;
+#include "C_XYWH_P.h"
+
+using namespace XYWH;
 
 Editor::Editor(Audio_Processor& processor, Data_Hub* hub) :
-    Editor_A{ processor, hub },
-    ctrls_aux_voice{ hub }/*,*/
+    Editor_A{ processor, hub }/*,*/
+    //ctrls_aux_voice{ hub },
     //layer_envelopes{ hub }
 {
     //addAndMakeVisible(layer_envelopes);
     //layer_envelopes.toBehind(ctrls_exp);
-    addAndMakeVisible(ctrls_aux_voice);
+    //addAndMakeVisible(ctrls_aux_voice);
     resized();
 }
 
 void Editor::resized() {
-    scale_factor = (float)getWidth() / GET::init_w_for(gui_editor);
+    scale_f = (float)getWidth() / editor_init_w;
     auto bounds = getLocalBounds();
     //layer_envelopes.setBounds(bounds);
     ctrls_exp.setBounds(bounds);
-    ctrls_aux_voice.setBounds(bounds);
+    //ctrls_aux_voice.setBounds(bounds);
     //app_options.set_scale_percentage_excluding(roundToInt(scale_factor * 100), this);
 }
--- P-600-GUI/P-600-GUI 2026/_Project_Source/D_X_G_Editor_P.cpp
+++ P-600-GUI/P-600-GUI 2026/_Project_Source/D_X_G_Editor_P.cpp
@@ -1,9 +1,8 @@
 #include "D_X_G_Editor_P.h"
 
-#include "C_ID_A.h"
-#include "C_GET_P.h"
+#include "C_XYWH_P.h"
 
-using namespace ID;
+using namespace XYWH;
 
 Editor::Editor(Audio_Processor& processor, Data_Hub* hub) :
     Editor_A{ processor, hub }
@@ -12,8 +11,7 @@ Editor::Editor(Audio_Processor& processor, Data_Hub* hub) :
 }
 
 void Editor::resized() {
-    scale_factor = (float)getWidth() / GET::init_w_for(gui_editor);
+    scale_f = (float)getWidth() / editor_init_w;
     auto bounds = getLocalBounds();
     ctrls_exp.setBounds(bounds);
-    //app_options.set_scale_percentage_excluding(roundToInt(scale_factor * 100), this);
 }
